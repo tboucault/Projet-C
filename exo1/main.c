d41cd84b17a42fa98d1a4a69025dd5846baa7540
tboucault <boucault.thomas@gmail.com>
2016-09-27 16:10:38 +0200

    fonctions

--- exo1/main.c
+++ exo1/main.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int cube(int nombre)
 {
-  float r1,r2,r3,resultats,resultatp=0;
-  int choix=0;
+    return nombre*nombre*nombre;
+}
 
-  printf("Montage serie (tapez 1) ou parallele (tapez 2)\n");
-  scanf("%d",&choix);
-
-      printf("saisir r1:\n");
-      scanf("%f",&r1);
-      printf("saisir r2:\n");
-      scanf("%f",&r2);
-      printf("saisir r3:\n");
-      scanf("%f",&r3);
-
-      if (choix==1){
-    resultats=(r1+r2+r3);
-    printf("La resistance equivalente est:%f",resultats);
-    }
-    else if(choix==2){
-    resultatp=(r1*r2*r3)/(r1*r2+r1*r3+r2*r3);
-    printf("La resistance equivalente est:%f",resultatp);
-    }
+void main()
+{
+    int nombreEntre = 0, resultat = 0;
+    printf("Entrez un nombre... ");
+    scanf("%d", &nombreEntre);
+    resultat = cube(nombreEntre);
+    printf("Le cube de %d est %d\n", nombreEntre,resultat);
 
 }
