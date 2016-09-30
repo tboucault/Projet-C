c56c2a7c92981735f0c924e95a1677a281a7155f
tboucault <boucault.thomas@gmail.com>
2016-09-30 10:02:37 +0200

    ds

--- exo1/main.c
+++ exo1/main.c
@@ -5,7 +5,7 @@
 
 // DS M15 LPAII 16/10/2014
 
-/* exo 1
+/* exo 1 */
 int add(int it[],int nel){
     int i,is=0;
     for(i=0;i<nel;i++)
@@ -14,18 +14,17 @@ int add(int it[],int nel){
         }
     return is;
     }
-
 void main(void)
 {
     int i1=2,i2=6,i=0,n;
     int it[]={2,3,4};
     if(i1==2)
-        printf("test 1 ok\n");
+        printf("test1: ok\n");
     else
-        puts("test1 ko");
+        puts("test1: ko");
     switch(i1){
-        case 1:case 3: case 5: puts("test2 OK");break;
-        case 2:case 4: case 6: puts("test2 ko");break;
+        case 1:case 3: case 5: puts("test2: OK");break;
+        case 2:case 4: case 6: puts("test2: ko");break;
         default:puts("test2:autre");
         }
     printf("test3: ");
@@ -34,11 +33,10 @@ void main(void)
         printf("%d",i);
         }while(i<=N);
     n=add(it,3);
-    printf("\ntest4:%d\n",n);
-}*/
+    printf("\ntest4: %d\n",n);
+}
 
-/* exo2
-
+/* exo2 */
 void main(void)
 {
     int valeur=1;
@@ -49,59 +47,47 @@ void main(void)
         i++;
     }while(valeur<0);
     printf("%d valeurs saisies\n",i);
+}
 
-}*/
-
-/* exo3
-
+/* exo3 */
 void main(void)
 {
     int chiffre,i,val;
-
     printf("Saisir valeur entre 1 et 9:");
     scanf("%d",&chiffre);
-
     for(i=chiffre;i<11;i++)
     {
         val=chiffre*10;
         printf("%d ",val);
         chiffre++;
     }
-}*/
+}
  /* exo 4 NE MARCHE PAS
-
 void main(void)
 {
     int somme,annee,i,taux;
     float valeur;
-
     printf("Donner la somme a emprunter:");
     scanf("%d",&somme);
     printf("Donner le taux d'interet en %:");
     scanf("%d",&taux);
     printf("Au bout decombien de temps vous commencez le remboursement:");
     scanf("%d",&annee);
-
-
         printf("%d",somme);
         printf("%d",taux);
         printf("%d",annee);
         valeur=(1+(3/100));
-
     printf("\nVous devrez rembourser %f euros",valeur);
 }*/
 
-/* exo 5,6,7
-
+/* exo 5,6,7 */
 void affichecarre(int n);
 void affichetable(int n);
-
 void main(void)
 {
     int choix;
     //affichetable(choix);
     //printf("\n");
-
     do{
         scanf("%d",&choix);
         affichecarre(choix);
@@ -124,22 +110,18 @@ void affichecarre(int n){
             printf("\n");
             j++;
     }while(j<n);
-}*/
+}
 
-/* exo 8,9,10,11
-
+/* exo 8,9,10,11 */
 void affiche(int t[], int taille);
 void miroir(int src[], int taille);
 void efface(int src[], int taille);
-
 void main(void)
 {
     int choix,a,b,c,d;
     int t[]={10,11,0,13};
-
     printf("Quelle fonction?\n1:affiche\n2:miroir\n3:efface\n");
     scanf("%d",&choix);
-
     if(choix==1)
     {
         affiche(t,4);
@@ -158,7 +140,6 @@ void main(void)
     }
     else
         printf("Choix incorrect\n");
-
 }
 void affiche(int t[], int taille)
 {
@@ -186,7 +167,4 @@ void efface(int src[], int taille)
         if(src[i]!=0)
             printf("%d ",src[i]);
     }
-}*/
-
-
-
+}
