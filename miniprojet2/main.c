9b8211beee721ca1d31420dad1176c22be425fb6
tboucault <boucault.thomas@gmail.com>
2016-10-08 23:03:10 +0200

    Miniprojet2 exo1 Final

--- miniprojet2/main.c
+++ miniprojet2/main.c
@@ -7,12 +7,14 @@ void main (void)
  {
      char tab [100];
      char format[3];
-     int i=0,j=0,k=0,l=0,m=0,n=0;
+     int i=0,j=0,k=0,l=0,m=0,n=0,ilongueur=0,ilargeur=0;
      char c;
      char longueur[3];
      char largeur[3];
      char contenu[10];
      char intensite[3];
+     char pixel[4];
+     int seuil;
      FILE  *Image_Origine = NULL ,*Image_Nouvelle = NULL ;
      Image_Origine  =  fopen ("C:\\Users\\Cyralik\\Documents\\GitHub\\Projet-C\\miniprojet2\\bin\\Debug\\lm1test.pgm", " r"); // ouverture fichier source
      Image_Nouvelle  =  fopen ("C:\\Users\\Cyralik\\Documents\\GitHub\\Projet-C\\miniprojet2\\bin\\Debug\\lm1new.pgm", " w+"); // ouverture/création fichier destination
@@ -27,61 +29,82 @@ void main (void)
 
             if(ispgm(Image_Origine,Image_Nouvelle)==1) // si fichier pgm, on continu
             {
-                    printf("   - Fichier .pgm; OK\n");
+                printf("   - Fichier .pgm; OK\n");
 
+                fgets ( tab , 100 , Image_Origine );
+                sscanf(tab,"%s",&contenu);
+                fputs(tab,Image_Nouvelle );
+                fgets ( tab , 100 , Image_Origine );
+                sscanf(tab,"%s",&contenu);
+                fputs(tab,Image_Nouvelle );
+
+                while(contenu[0]=='#') //si ligne de commentaire, on saute de ligne
+                {
                     fgets ( tab , 100 , Image_Origine );
                     sscanf(tab,"%s",&contenu);
-                    fputs(tab,Image_Nouvelle );
-                    fgets ( tab , 100 , Image_Origine );
-                    sscanf(tab,"%s",&contenu);
-                    fputs(tab,Image_Nouvelle );
+                    fputs(tab,Image_Nouvelle );//ecriture des lignes de commentaire dans le nouveau fichier
+                }
 
-                    while(contenu[0]=='#') //si ligne de commentaire, on saute de ligne
+                               //lecture de la taille du tableau
+                do{
+                    c=tab[i++];
+                }while(c!=' ');// detection de l'espace entre longueur et largeur
+                do{
+                    c=tab[l++];
+                }while(c!='\n');// detection fin de largeur
+
+                for(j=0;j<i-1;j++)
+                {
+                    longueur[j]=tab[j];//stockage longueur (valeur avant l'espace)
+                }
+                for(k=i;k<l-1;k++)
+                {
+                    largeur[m]=tab[k];//stockage largeur (valeur apres l'espace)
+                    m++;
+                }
+                printf("   - Format de l'image: OK\n");
+
+                fgets ( tab , 100 , Image_Origine ); //stockage intensité max
+                sscanf(tab,"%s",&intensite);
+                fputs(tab,Image_Nouvelle );// ecriture ligne intensité dans nouveau fichier
+                printf("   - Intensite de l'image: OK\n");
+
+                /* trouver une methode pour copier le fichier selon longueur et largeur prédéfini (dynamique)
+                sscanf(longueur,"%d",&ilongueur);
+                sscanf(largeur,"%d",&ilargeur);*/
+                // demande du seuil à l'utilisateur
+                printf("Valeur du seuil: \n");
+                scanf("%d",&seuil);
+
+                for (n=0; n<(512*512); n++)
+                {
+                    fgets(tab ,100 ,Image_Origine );//extraction de la valeur d'un pixel du fichier
+                    sscanf(tab,"%s",&pixel);//stockage de la valeur dans le tableau pixel[]
+                    if(atoi(pixel)<seuil)
                     {
-                        fgets ( tab , 100 , Image_Origine );
-                        sscanf(tab,"%s",&contenu);
-                        fputs(tab,Image_Nouvelle );//ecriture des lignes de commentaire dans le nouveau fichier
+                        sprintf(pixel,"%s","0\n");
+                        fputs(pixel,Image_Nouvelle );
                     }
-
-                                   //lecture de la taille du tableau
-                    do{
-                        c=tab[i++];
-                    }while(c!=' ');// detection de l'espace entre longueur et largeur
-                    do{
-                        c=tab[l++];
-                    }while(c!='\n');// detection fin de largeur
-
-                    for(j=0;j<i-1;j++)
+                    else if(atoi(pixel)>=seuil)
                     {
-                        longueur[j]=tab[j];//stockage longueur (valeur avant l'espace)
+                        sprintf(pixel,"%s","255\n");
+                        fputs(pixel,Image_Nouvelle );
                     }
-                    for(k=i;k<l-1;k++)
-                    {
-                        largeur[m]=tab[k];//stockage largeur (valeur apres l'espace)
-                        m++;
-                    }
-                    printf("   - Format de l'image: OK\n");
+                    else
+                        printf("Erreur comparaison seuil\n"); //message d'erreur
+                }
+                printf("   - Duplication de l'image: OK\n");
 
-                    fgets ( tab , 100 , Image_Origine ); //stockage intensité max
-                    sscanf(tab,"%s",&intensite);
-                    fputs(tab,Image_Nouvelle );// ecriture ligne intensité dans nouveau fichier
-                    printf("   - Intensite de l'image: OK\n");
+                //binarisation de l'image
 
                 // Recopie  dans  le  fichier  destination
-                for (n=0; n<(512*512); n++)
+               /* for (n=0; n<(512*512); n++)
                 {
                     fgets(tab ,100 ,Image_Origine );
                     fputs(tab,Image_Nouvelle );
-                }
-                // Creation  d’ un  tab  dynamique  de  la  longueur  d’ une  ligne  ( variable  a  declarer )
-                // Algo
-                /*for (...)
-                {
-                    // On  recopie  toute  la ieme  ligne  dans le tableau
-                    // on  inverse  la  ligne  dans  Tab
-                    // on  recopie  Tab  dans  le  fichier  de  destination
                 }*/
-                // Puis  on   ferme   les  fichiers
+                printf("   - Copie dans le fichier de destination: OK\n");
+                // Fermeture des fichiers
                 fclose ( Image_Origine );
                 fclose ( Image_Nouvelle );
             }
